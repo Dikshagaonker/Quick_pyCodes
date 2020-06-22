b119876725afa0a8d55c221a45b44d9da79b3d66
Dikshagaonker <31814177+Dikshagaonker@users.noreply.github.com>
2020-06-22 23:58:46 +0530

    Create Simple_GUI.py
    This is a simple graphical interface created to display the image pattern.

---
+++ Simple_GUI.py
@@ -0,0 +1,18 @@
+#OUR SIMPLE FIRST GUI
+
+picture = [
+     [0,0,0,1,0,0,0],
+     [0,0,1,1,1,0,0],
+     [0,1,1,1,1,1,0],
+     [1,1,1,1,1,1,1],
+     [0,0,0,1,0,0,0],
+     [0,0,0,1,0,0,0],
+      ]
+
+for row in picture:
+    for value in row:
+        if value == 1:
+            print('*', end = ' ')
+        else:
+            print(' ', end = ' ')
+    print(' ')
